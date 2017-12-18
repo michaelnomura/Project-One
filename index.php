cb0733228b35075cfa7648631dcfa161990ca4e6
Michael Nomura <mnnomura@gmail.com>
2017-12-18 12:42:04 -0800

    Added Nav

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <style>
+    ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 20%;
+    background-color: #ff9999;
+    height: 100%; 
+    position: fixed;
+    top: 0;
+    left: 0;
+    overflow: auto; 
+        }
+    li {
+    margin:3%;
+        }
+    </style>
     <title></title>
     </head>
 <body>
+    <ul>
+  <li><a class="active" href="#home">Index</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+    <div style="margin-left:20%;padding:1px 10px;height:1000px;">
     <h1>Long Live the Internet</h1>
     <p>Text</p>
+    </div>
     </body>
     </html>
